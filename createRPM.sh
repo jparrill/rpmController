2ff57908ecc7dbc12a968bf37e1aa7c827151f42
troitino <troitino@fisherman.hi.inet>
2013-10-12 08:58:44 +0200

    I added the directories needed to make the rpms in the createrepo.sh

--- createRPM.sh
+++ createRPM.sh
@@ -1,1 +1,3 @@
+mkdir -p BUILD RPMS SOURCES SPECS SRPMS
+
 /usr/bin/rpmbuild --buildroot `pwd`/BUILDROOT/ --define "_sourcedir `pwd`" --define "_rpmdir ./RPMS" --define "_buildir ." --define "_srcrpmdir ." --define "_speccdir ." --define "_topdir ." --define "_hash 12345" --define "_jobs 01" --define "_changelog 12345" -ba SPECS/rpmControler.spec
