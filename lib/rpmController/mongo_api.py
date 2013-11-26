f9a8b767159fb0ea480007f9fcfd911ad482d5a8
Juan Manuel Parrilla Madrid <padajuan@gmail.com>
2013-11-26 13:42:16 +0100

    Changed logging mode, now in libraries

--- lib/rpmController/mongo_api.py
+++ lib/rpmController/mongo_api.py
@@ -16,7 +16,12 @@ class Info(object):
   def get_config(self):
     ## Catch config from config file
     config = ConfigParser.RawConfigParser()
-    config.read('../conf/rpmController.ini')
+    try:
+      logging.debug('Reading configuration')
+      config.read('../conf/rpmController.ini')
+    except:
+      logging.critital('Error reading config file')
+
     self.ip_mongo = config.get('mongo', 'ip')
     self.port_mongo = config.getint('mongo','port')
     self.db_mongo = config.get('mongo', 'database')
@@ -43,14 +48,22 @@ class Info(object):
   def deleter(self, collection, rpm, status):
     ## Put deletef field of collection to true or false
     db = self.mongo_con(self.ip_mongo, self.port_mongo, self.db_mongo)
-    code = db[collection].update({"name":rpm["name"], "version":rpm["version"], "release":rpm["release"]}, {'$set': {"deleted": status}})
+    try:
+      logging.debug('modifying RPM in MongoDB: %s', rpm['name'])
+      code = db[collection].update({"name":rpm["name"], "version":rpm["version"], "release":rpm["release"]}, {'$set': {"deleted": status}})
+    except:
+      logging.critical('Error updating %s', rpm['name'])
     return code
 
   def collection_maker(self, collection, info_host, packages):
     ## Make a new collection if not exists
     db = self.mongo_con(self.ip_mongo, self.port_mongo, self.db_mongo)
-    db[collection].insert(info_host)
-    db[collection].insert(packages)
+    try:
+      logging.debug('Making new collection')
+      db[collection].insert(info_host)
+      db[collection].insert(packages)
+    except:
+      logging.error('Error Making new collection %s', collection)
     
   def finder(self, collection, key, pattern):
     ## finder api 
@@ -63,9 +76,15 @@ class Info(object):
     ## Catch all packages of MongoDB collection
     packages = []
     db = self.mongo_con(self.ip_mongo, self.port_mongo, self.db_mongo)
-    for item in db[collection].find({"deleted":{'$exists': True}}):
-      packages.append(item) 
-    db[collection].disconnect
+    try:
+      logging.debug('Getting packages of %s', collection)
+      for item in db[collection].find({"deleted":{'$exists': True}}):
+        packages.append(item) 
+      db[collection].disconnect
+
+    except:
+      logging.error('Error getting packages')
+      raise
     return packages
 
   def print_collection(self, db_collection):
@@ -73,7 +92,7 @@ class Info(object):
       if collection_ != "system.indexes":
         print collection_
 
-  def print_info_host(self,fqdn):
+  def print_info_host(self, fqdn):
     db = self.mongo_con(self.ip_mongo, self.port_mongo, self.db_mongo)
     rpms=db[fqdn]
     for host in rpms.find({"fqdn": "localhost.localdomain"}):
@@ -82,7 +101,7 @@ class Info(object):
       # because record is a dict, we get you use lots of python magic
       print "id=%(_id)s date=%(date)s version=%(version)s review_date=%(review_date)s name=%(name)s deleted=%(deleted)s release=%(release)s" % record  
 
-  def print_info_rpms(self,fqdn,ip):
+  def print_info_rpms(self, fqdn, ip):
     db = self.mongo_con(self.ip_mongo, self.port_mongo, self.db_mongo)
     rpms=db[fqdn]
     for host in rpms.find({"ip": ip, "SO_release":{'$exists': True}}):
