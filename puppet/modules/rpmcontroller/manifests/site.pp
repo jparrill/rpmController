fc587c866b7e140167f8012fb13568eb3b059caa
troitino <troitino@fisherman.hi.inet>
2013-12-04 11:34:17 +0100

    add site to puppet module rpmcontroller 3

---
+++ puppet/modules/rpmcontroller/manifests/site.pp
@@ -0,0 +1,41 @@
+ 
+$path_to_repo_rpmcontroller=http://artifacts.hi.inet/others/rpmController/6.X/x86_64/
+$ip=ci-rmtest.hi.inet
+$port=27017
+$database=rpmdb
+$path_to_config=/opt/rpmcontroller/conf/rpmController.ini
+
+
+
+File {
+      seluser  => 'system_u',
+      selrole  => 'object_r',
+      seltype  => 'etc_t',
+      selrange => 's0',
+}
+  
+
+yumrepo { 'repo_rpmcontroller':
+    descr    => 'Repo general del rpmcontroller',
+    enabled  => 1,
+    gpgcheck => 0,
+    baseurl  => $path_to_repo_rpmcontroller,
+    priority => 1,
+   } 
+->
+exec { 'yum-clean-expire-cache':
+    user => 'root',
+    path => '/usr/bin',
+    command => 'yum clean expire-cache',
+  }
+->
+package { 'rpmcontroller':
+  ensure  => latest,
+  require => Yumrepo['repo_rpmcontroller'],
+}    
+->    
+file { 'config_rpmcontroler':
+  ensure  => file,
+  path  => $path_to_config,
+  content => template('rpmcontroller/rpmController.erb'),    
+}
